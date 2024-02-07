1d7c8a40181ddf10ed5b568a65becf08b2f6431a
ssong94 <kyugwang@gmail.com>
2024-02-07 23:34:31 +0900

    init

---
+++ src/main/java/com/spring/boot/excelparser/ExcelParserApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.excelparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelParserApplication.class, args);
+	}
+
+}
