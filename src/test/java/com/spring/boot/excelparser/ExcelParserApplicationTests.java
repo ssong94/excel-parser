1e291f4dab090e1fc385254484846ad0894a74c6
ssong94 <kyugwang@gmail.com>
2024-02-15 00:12:31 +0900

    test: 테스트 클래스 추가
    - 수정하려면 파일의 path만 수정하면 된다.

--- src/test/java/com/spring/boot/excelparser/ExcelParserApplicationTests.java
+++ src/test/java/com/spring/boot/excelparser/ExcelParserApplicationTests.java
@@ -1,13 +1,53 @@
 package com.spring.boot.excelparser;
 
+import com.spring.boot.excelparser.vo.Error;
+import com.spring.boot.excelparser.vo.ExcelResult;
+import com.spring.boot.excelparser.vo.TestSheet;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootTest
-class ExcelParserApplicationTests {
+class ExcelExcelParserApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+
+	@Test
+	void test() throws IOException {
+
+		Path path = Paths.get("C:\\Users\\ssong94\\바탕 화면\\TestExcel.xlsx");
+		String name = "file.xlsx";
+		String originalFileName = "file.xlsx";
+		String contentType = "application/vnd.ms-excel";
+		byte[] content = null;
+		try {
+			content = Files.readAllBytes(path);
+		} catch (final IOException e) {
+		}
+		MultipartFile mf = new MockMultipartFile(name,
+				originalFileName, contentType, content);
+
+
+		ExcelResult<TestSheet> result = ExcelParser.parse(mf, TestSheet.class);
+
+		List<Error> errors = result.errors();
+		List<String> headerNames = result.headerNames();
+		String sheetName = result.sheetName();
+		List<TestSheet> successData = result.successData();
+
+		System.out.println("success");
+
+
+	}
+
 
 }
